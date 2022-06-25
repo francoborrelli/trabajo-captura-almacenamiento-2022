8626e7370eefcea2e7966632b93c686e4d16f925
Franco Martin Borrelli <francoborrelli96@outlook.com>
2022-06-25 17:25:16 -0300

    improve benchmark results

--- benchmark/index.js
+++ benchmark/index.js
@@ -6,39 +6,94 @@ const services = {
   postgres: 'http://api:8080/api/postgres/products/search/',
 };
 
-const rawdata = fs.readFileSync('./input/words.json');
+const benchmarkOptions = {
+  debug: true, // (Boolean, default false): Displays some info on the console.
 
-const { results: words } = JSON.parse(rawdata);
+  runMode: 'sequence', // (String, default 'sequence'): Can be 'sequence' (each request is made after receiving the previous response) or 'parallel' (all requests are made in parallel).
 
-const routes = words.slice(0, 50).reduce((prev, curr) => {
-  prev[curr.word] = curr.word;
-  return prev;
-}, {});
+  minSamples: 20, // (Number, default 20): The minimum sample size required to perform statistical analysis.
+};
 
-console.log(' ---------------------------------------');
-console.log('|           COMENZANDO PRUEBA           |');
-console.log(' ---------------------------------------');
+const showMessage = (message) => {
+  console.log(' ---------------------------------------');
+  console.log(`|           ${message.toUpperCase()}           |`);
+  console.log(' ---------------------------------------');
+};
 
-apiBenchmark.measure(
-  services,
-  routes,
-  { debug: true, runMode: 'sequence', minSamples: 20 },
-  function (err, results) {
-    console.log(' ---------------------------------------');
-    console.log('|           PRUEBA FINALIZADA           |');
-    console.log(' ---------------------------------------');
+const getRoutes = () => {
+  const rawdata = fs.readFileSync('./input/words.json');
+  const { results: words } = JSON.parse(rawdata);
+  return words.slice(0, 20).reduce((prev, curr) => {
+    prev[curr.word] = curr.word;
+    return prev;
+  }, {});
+};
 
-    // se guarda el json con resultados
-    fs.writeFile('/app/output/results.json', JSON.stringify(results), (err) => {
-      if (err) throw err;
-      console.log('File successfully written to disk');
+const saveFile = (filename, content) => {
+  fs.writeFileSync(filename, content, (err) => {
+    if (err) throw err;
+  });
+};
+
+const saveJSONResult = (results) => {
+  saveFile('/app/output/results.json', JSON.stringify(results));
+};
+
+const saveDetailsHTMLResult = (results) => {
+  Object.keys(results).forEach((server) => {
+    Object.keys(results[server]).forEach((route) => {
+      if (results[server][route].response) {
+        results[server][route].response.body = null;
+      }
     });
+  });
+  apiBenchmark.getHtml(results, function (error, html) {
+    saveFile('/app/output/report.html', html);
+  });
+};
 
-    // se guarda el reporte html con resultados
-    apiBenchmark.getHtml(results, function (error, html) {
-      fs.writeFile('/app/output/report.html', html, (err) => {
-        if (err) throw err;
-      });
-    });
-  }
-);
+const getGeneralStats = (results) => {
+  // calcula el promedio de tiempo de respuesta para todos los resultados del arreglo
+  const calculateAvg = (data) =>
+    data.filter((s) => s.stats && s.stats.mean).reduce((prev, curr) => prev + curr.stats.mean, 0) /
+    data.filter((s) => s.stats && s.stats.mean).length;
+
+  // se queda con aquellos resultados donde resultó la respuesta más rápida.
+  const calculateWinnings = (data) => data.filter((r) => r.isFastest);
+
+  // calcula el promedio de tiempo de respuesta para todos los resultados del arreglo
+  const calculateStats = (results) => {
+    return [calculateAvg(results), calculateWinnings(results)];
+  };
+
+  const [avgElasticTime, elasticWinnings] = calculateStats(Object.values(results.elastic));
+  const [avgPostgresTime, postgrestWinnings] = calculateStats(Object.values(results.postgres));
+
+  showMessage('Resultado general');
+  console.log(`------------------------------------------------------------------`);
+
+  console.log(`Resultados Elasticsearch:`);
+  console.log(`Fue más rápido en: ${elasticWinnings.length} términos.`);
+  console.log(`Tuvo un tiempo promedio de respuesta de : ${avgElasticTime} seg.`);
+
+  console.log(`------------------------------------------------------------------`);
+
+  console.log(`Resultados Postgres:`);
+  console.log(`Fue más rápido en: ${postgrestWinnings.length} términos.`);
+  console.log(`Tuvo un tiempo promedio de respuesta de : ${avgPostgresTime} seg.`);
+
+  console.log(`------------------------------------------------------------------`);
+};
+
+const runBenchmark = () => {
+  showMessage('Comenzando prueba');
+  const routes = getRoutes();
+  apiBenchmark.measure(services, routes, benchmarkOptions, function (err, results) {
+    showMessage('Prueba finalizada');
+    saveJSONResult(results);
+    saveDetailsHTMLResult(results);
+    getGeneralStats(results);
+  });
+};
+
+runBenchmark();
